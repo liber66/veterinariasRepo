ce4d8a3086f4c9e789c7e7ddf9566c6683b2ec46
Liber <liber66@hotmail.com>
2014-04-22 04:07:37 +0200

    jb

--- VeterinariasGUI/src/veterinarias/mascotas/modals/IngresarFichaClinica.java
+++ VeterinariasGUI/src/veterinarias/mascotas/modals/IngresarFichaClinica.java
@@ -1,33 +1,46 @@
 package veterinarias.mascotas.modals;
 
 import java.awt.Font;
+import java.util.Date;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.Group;
 import javax.swing.GroupLayout.ParallelGroup;
 import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 
 import veterinarias.mascotas.actions.BotonConfirmarFichaClinicaAction;
 import veterinarias.socios.jframes.NuevasMascotas;
 
+import com.toedter.calendar.JDateChooser;
+
 public class IngresarFichaClinica extends JPanel {
 
     private static final long serialVersionUID = 1L;
     private GroupLayout gl_contentPane;
-    private JScrollPane scrPnlInformacion;
+    private JTabbedPane tabbedPane;
+    private JButton btnConfirmar;
     //Atributos
     private NuevasMascotas nuevasMascotas;
     private JDialog dialog;
-    //Componentes
+    //Componentes de una pestaña
+    private JPanel panelPestaniaInicial;
+    private GroupLayout gl_panelPestaniaInicial;
+    private JLabel lblFecha;
+    private JDateChooser dateChooser;
+    private JScrollPane scrPnlInformacion;
     private JTextArea txtInformacion;
-    private JButton btnConfirmar;
     //Acciones
     private BotonConfirmarFichaClinicaAction btnConfirmarFichaClinicaAction = new BotonConfirmarFichaClinicaAction(this);
+    //Constantes
+    private static String SIMBOLO_AGREGAR_PESTANIA = "+";
 
     /**
      * Create the panel.
@@ -40,6 +53,26 @@ public class IngresarFichaClinica extends JPanel {
     }
 
     protected void initComponents() {
+        //Inicializo TabbedPane y le agrego las pestañas
+        tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        panelPestaniaInicial = new JPanel();
+        tabbedPane.addTab("New tab", null, panelPestaniaInicial, null);
+        lblFecha = new JLabel("Fecha:");
+        lblFecha.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+        dateChooser = new JDateChooser();
+        dateChooser.setDate(new Date());
+        //Agrego al JScrollPAnel el area de texto
+        scrPnlInformacion = new JScrollPane();
+        scrPnlInformacion.setViewportView(txtInformacion);
+        gl_panelPestaniaInicial = new GroupLayout(panelPestaniaInicial);
+        panelPestaniaInicial.setLayout(gl_panelPestaniaInicial);
+        //Agrego al group layout de la pestaña inicial los componentes
+        gl_panelPestaniaInicial.setHorizontalGroup(horizontalGroupPanelInicial(gl_panelPestaniaInicial, lblFecha, dateChooser, scrPnlInformacion));
+        gl_panelPestaniaInicial.setVerticalGroup(verticalGroupPanelInicial(gl_panelPestaniaInicial, lblFecha, dateChooser, scrPnlInformacion));
+        //Agrego pestaña con simbolo "+" para poder agregar nuevas pestañas dinamicamente
+        JPanel panelAgregarPestaña = new JPanel();
+        panelAgregarPestaña.setName(SIMBOLO_AGREGAR_PESTANIA);
+        tabbedPane.addTab(SIMBOLO_AGREGAR_PESTANIA, null, null, null);
         //Inicializo componentes
         txtInformacion = new JTextArea(10, 50);
         txtInformacion.setText(nuevasMascotas.getInformacion());
@@ -48,9 +81,6 @@ public class IngresarFichaClinica extends JPanel {
         txtInformacion.setFont(new Font("Tahoma", Font.PLAIN, 12));
         btnConfirmar = new JButton("Confirmar");
         btnConfirmar.setAction(btnConfirmarFichaClinicaAction);
-        //Agrego al JScrollPAnel el area de texto
-        scrPnlInformacion = new JScrollPane();
-        scrPnlInformacion.setViewportView(txtInformacion);
         //Armo Group Layout del contentPane
         gl_contentPane = new GroupLayout(this);
         this.setLayout(gl_contentPane);
@@ -58,9 +88,21 @@ public class IngresarFichaClinica extends JPanel {
         gl_contentPane.setVerticalGroup(verticalGroupPanel());
     }
 
+    private Group horizontalGroupPanelInicial(GroupLayout groupLayout, JLabel lblFecha, JDateChooser dateChooser, JScrollPane scrPnlInformacion) {
+        ParallelGroup result = groupLayout.createParallelGroup(Alignment.LEADING);
+        //SequentialGroup Fecha
+        SequentialGroup sGroupFecha = groupLayout.createSequentialGroup();
+        return result;
+    }
+
+    private Group verticalGroupPanelInicial(GroupLayout groupLayout, JLabel lblFecha, JDateChooser dateChooser, JScrollPane scrPnlInformacion) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
     private ParallelGroup horizontalGroupPanel() {
         ParallelGroup result = gl_contentPane.createParallelGroup(Alignment.LEADING);
-        result.addComponent(scrPnlInformacion, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
+        result.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
         result.addGap(5);
         result.addComponent(btnConfirmar, Alignment.CENTER, GroupLayout.DEFAULT_SIZE, 100, 100);
         return result;
@@ -70,7 +112,7 @@ public class IngresarFichaClinica extends JPanel {
         ParallelGroup result = gl_contentPane.createParallelGroup(Alignment.LEADING);
         //SequentialGroup campo de texto y boton de confirmar
         SequentialGroup sGroupTablaSocios = gl_contentPane.createSequentialGroup();
-        sGroupTablaSocios.addComponent(scrPnlInformacion, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
+        sGroupTablaSocios.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
         sGroupTablaSocios.addGap(5);
         sGroupTablaSocios.addComponent(btnConfirmar, GroupLayout.DEFAULT_SIZE, 10, 10);
         sGroupTablaSocios.addGap(5);
