acd9b7912af4e6698467d4f36ca9689b64d1690e
Liber <liber66@hotmail.com>
2014-04-22 05:10:43 +0200

    asdasd

--- VeterinariasGUI/src/veterinarias/mascotas/modals/IngresarFichaClinica.java
+++ VeterinariasGUI/src/veterinarias/mascotas/modals/IngresarFichaClinica.java
@@ -1,6 +1,9 @@
 package veterinarias.mascotas.modals;
 
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Date;
 
 import javax.swing.GroupLayout;
@@ -15,6 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import veterinarias.mascotas.actions.BotonConfirmarFichaClinicaAction;
 import veterinarias.socios.jframes.NuevasMascotas;
@@ -53,14 +57,54 @@ public class IngresarFichaClinica extends JPanel {
     }
 
     protected void initComponents() {
+        //Inicializo componentes
+        txtInformacion = new JTextArea(10, 50);
+        txtInformacion.setText(nuevasMascotas.getInformacion());
+        txtInformacion.setLineWrap(true);
+        txtInformacion.setWrapStyleWord(true);
+        txtInformacion.setFont(new Font("Tahoma", Font.PLAIN, 12));
         //Inicializo TabbedPane y le agrego las pestañas
         tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        tabbedPane.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String nameComponent = tabbedPane.getSelectedComponent().getName();
+                if (nameComponent != null && nameComponent.equals("+")) {
+                    JPanel panel = new JPanel();
+                    Component c = tabbedPane.getComponent(tabbedPane.getTabCount() - 1);
+                    tabbedPane.addTab("New tab", null, panel, null);
+                    tabbedPane.setComponentAt(tabbedPane.getTabCount() - 2, panel);
+                    tabbedPane.add(c);
+                    //Creo componentes nuevos
+                    JLabel lblFecha = new JLabel("Fecha:");
+                    lblFecha.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
+                    JDateChooser dateChooser = new JDateChooser();
+                    dateChooser.setDate(new Date());
+                    ((JTextField) dateChooser.getDateEditor()).setEditable(false);
+                    JTextArea txtInformacion = new JTextArea(10, 50);
+                    txtInformacion.setText(nuevasMascotas.getInformacion());
+                    txtInformacion.setLineWrap(true);
+                    txtInformacion.setWrapStyleWord(true);
+                    txtInformacion.setFont(new Font("Tahoma", Font.PLAIN, 12));
+                    JScrollPane scrPnlInformacion = new JScrollPane();
+                    scrPnlInformacion.setViewportView(txtInformacion);
+                    //Agrego layout
+                    GroupLayout groupLayout = new GroupLayout(panel);
+                    panel.setLayout(groupLayout);
+                    groupLayout.setHorizontalGroup(horizontalGroupPanelInicial(groupLayout, lblFecha, dateChooser, scrPnlInformacion));
+                    groupLayout.setVerticalGroup(verticalGroupPanelInicial(groupLayout, lblFecha, dateChooser, scrPnlInformacion));
+                    tabbedPane.setSelectedComponent(panel);
+                }
+            }
+        });
         panelPestaniaInicial = new JPanel();
         tabbedPane.addTab("New tab", null, panelPestaniaInicial, null);
         lblFecha = new JLabel("Fecha:");
         lblFecha.setFont(new Font("Comic Sans MS", Font.PLAIN, 16));
         dateChooser = new JDateChooser();
         dateChooser.setDate(new Date());
+        ((JTextField) dateChooser.getDateEditor()).setEditable(false);
         //Agrego al JScrollPAnel el area de texto
         scrPnlInformacion = new JScrollPane();
         scrPnlInformacion.setViewportView(txtInformacion);
@@ -72,13 +116,8 @@ public class IngresarFichaClinica extends JPanel {
         //Agrego pestaña con simbolo "+" para poder agregar nuevas pestañas dinamicamente
         JPanel panelAgregarPestaña = new JPanel();
         panelAgregarPestaña.setName(SIMBOLO_AGREGAR_PESTANIA);
-        tabbedPane.addTab(SIMBOLO_AGREGAR_PESTANIA, null, null, null);
-        //Inicializo componentes
-        txtInformacion = new JTextArea(10, 50);
-        txtInformacion.setText(nuevasMascotas.getInformacion());
-        txtInformacion.setLineWrap(true);
-        txtInformacion.setWrapStyleWord(true);
-        txtInformacion.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        tabbedPane.addTab(SIMBOLO_AGREGAR_PESTANIA, null, panelAgregarPestaña, null);
+        //Agrego boton confirmacion
         btnConfirmar = new JButton("Confirmar");
         btnConfirmar.setAction(btnConfirmarFichaClinicaAction);
         //Armo Group Layout del contentPane
@@ -92,12 +131,34 @@ public class IngresarFichaClinica extends JPanel {
         ParallelGroup result = groupLayout.createParallelGroup(Alignment.LEADING);
         //SequentialGroup Fecha
         SequentialGroup sGroupFecha = groupLayout.createSequentialGroup();
+        sGroupFecha.addGap(20);
+        sGroupFecha.addComponent(lblFecha, 50, 50, 50);
+        sGroupFecha.addGap(20);
+        sGroupFecha.addComponent(dateChooser, 120, 120, 120);
+        //Result
+        result.addGroup(sGroupFecha);
+        result.addGap(20);
+        result.addComponent(scrPnlInformacion, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
         return result;
     }
 
     private Group verticalGroupPanelInicial(GroupLayout groupLayout, JLabel lblFecha, JDateChooser dateChooser, JScrollPane scrPnlInformacion) {
-        // TODO Auto-generated method stub
-        return null;
+        ParallelGroup result = groupLayout.createParallelGroup(Alignment.LEADING);
+        //ParallelGroup Fecha
+        ParallelGroup pGroupFecha = groupLayout.createParallelGroup(Alignment.LEADING);
+        pGroupFecha.addGap(20);
+        pGroupFecha.addComponent(lblFecha, 20, 20, 20);
+        pGroupFecha.addGap(40);
+        pGroupFecha.addComponent(dateChooser, 25, 25, 25);
+        //SequentialGroup informacion
+        SequentialGroup sGroupInformacion = groupLayout.createSequentialGroup();
+        sGroupInformacion.addGap(20);
+        sGroupInformacion.addGroup(pGroupFecha);
+        sGroupInformacion.addGap(10);
+        sGroupInformacion.addComponent(scrPnlInformacion, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
+        //Result
+        result.addGroup(sGroupInformacion);
+        return result;
     }
 
     private ParallelGroup horizontalGroupPanel() {
